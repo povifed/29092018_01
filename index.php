fafc3f67b3490e7c97e2686dec20d2e781f3c84b
povifed <polina.fedoseeva2014@ya.ru>
2019-02-03 12:12:51 +0300

    Comment

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php 
-    echo "Я в git";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel = "stylesheet" href ="/css/style.css">
+    <title>Главная страница</title>
+</head>
+<body>
+    <h1>Это наша супер-страница</h1>
+    <script src = "/js/main.js"></script>
+</body>
+</html>
